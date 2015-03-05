21ee69764a5ca64e9aae1391b0f42614a2eda8c2
Frank Yu <frankisblissful@gmail.com>
2015-03-04 23:10:39 -0800

    added a socket.io chat example

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 var express = require('express');
 var router = express.Router();
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(__dirname + '/../views/index.html');
 });
 
 module.exports = router;
+
+
+
