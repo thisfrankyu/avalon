657a2a48826d5bce0e46d3468e459273ae16a800
Phoebe Simon <phoebesimon@hotmail.com>
2015-03-16 00:35:13 +0000

    Got the socket.io chat client running on Google Hangouts. (And now serving xml from a Rackspace box)
    certs/: self-signed certs
    index.html: moved to public/
    index.js: use https
    avalon.xml: xml for hangouts app
    index.html: moved to public/

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+var fs = require('fs');
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var privateKey = fs.readFileSync('certs/key.pem', 'utf8');
+var certificate = fs.readFileSync('certs/cert.pem', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+var https = require('https');
+var server = https.createServer(credentials, app);
+var io = require('socket.io')(server);
 var SessionController = require('./session/session').SessionController;
 var GameController = require('./game/game-controller').GameController;
 var newEmitter = require('./communication/emitter').newEmitter;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+app.get('/avalon.xml', function(req, res){
+  res.sendFile(__dirname + '/public/avalon.xml');
 });
 var emitter = newEmitter();
 var sessionController = new SessionController(emitter, io);
@@ -17,7 +26,7 @@ emitter.on('error', function (error) {
   console.log('error caught', error);
 });
 
-var port = 3005;
-http.listen(port, function(){
-  console.log('listening on *:'+port);
+var port = 3000;
+server.listen(port, function(){
+  console.log('listening on *:' + port);
 });
