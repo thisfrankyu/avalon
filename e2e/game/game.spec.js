1de00a440790e17e697dc6ecace1c87a6b2ea91f
Frank Yu <frankisblissful@gmail.com>
2015-03-21 20:56:57 -0700

    added protractor skeleton for starting a game

---
+++ e2e/game/game.spec.js
@@ -0,0 +1,31 @@
+'use strict';
+
+describe('stuff View', function () {
+  var page,
+    _ = require('underscore'),
+    CreateGamePage = require('./createGame.po'),
+    JoinGamePage = require('./joinGame.po'),
+    joinGameBrowsers = [],
+    ownerId = 'player0',
+    gameId = 'game0';
+  beforeEach(function () {
+    browser.get('/createGame');
+    page = new CreateGamePage(browser);
+    page.playerIdInput.sendKeys(ownerId);
+    page.gameIdInput.sendKeys(gameId);
+    page.submitButton.click();
+    _.times(5, function (i) {
+      var joinGameBrowser = browser.forkNewDriverInstance(true),
+        joinGamePage = new JoinGamePage(joinGameBrowser);
+      joinGameBrowser.get('/joinGame');
+      joinGameBrowsers.push[joinGameBrowser];
+      joinGamePage.playerIdInput.sendKeys('player'+(i+1));
+      joinGamePage.gameIdInput.sendKeys(gameId);
+      joinGamePage.submitButton.click();
+    });
+  });
+
+  it('should do stuff', function () {
+    browser.sleep(100000);
+  });
+});
