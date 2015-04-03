a4fca107dcc2909c67a58cc5a9bf50ce655064d8
Frank Yu <frankisblissful@gmail.com>
2015-04-02 19:29:12 -0700

    moved index.js into chat-console and added a new package.json that hopefully works

--- chat-console/index.js
+++ chat-console/index.js
@@ -1,21 +1,21 @@
 var fs = require('fs');
 var app = require('express')();
 var http = require('http').Server(app);
-var privateKey = fs.readFileSync('certs/key.pem', 'utf8');
-var certificate = fs.readFileSync('certs/cert.pem', 'utf8');
+var privateKey = fs.readFileSync('../certs/key.pem', 'utf8');
+var certificate = fs.readFileSync('../certs/cert.pem', 'utf8');
 var credentials = {key: privateKey, cert: certificate};
 var https = require('https');
 var server = https.createServer(credentials, app);
 var io = require('socket.io')(server);
-var SessionController = require('./session/session').SessionController;
-var GameController = require('./game/game-controller').GameController;
-var newEmitter = require('./communication/emitter').newEmitter;
+var SessionController = require('../session/session').SessionController;
+var GameController = require('../game/game-controller').GameController;
+var newEmitter = require('../communication/emitter').newEmitter;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 app.get('/avalon.xml', function(req, res){
-  res.sendFile(__dirname + '/public/avalon.xml');
+  res.sendFile(__dirname + '/avalon.xml');
 });
 var emitter = newEmitter();
 var sessionController = new SessionController(emitter, io);
