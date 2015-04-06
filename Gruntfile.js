a39fef73d4fa71e328d9161b6174f950f1cf63fc
Kevin Arthur <thetoothpick@gmail.com>
2015-04-06 15:55:57 -0700

    change avalon.xml path in app.js to work in dist mode

--- Gruntfile.js
+++ Gruntfile.js
@@ -357,6 +357,7 @@ module.exports = function (grunt) {
             'bower_components/**/*',
             'assets/images/{,*/}*.{webp}',
             'assets/fonts/**/*',
+            'avalon.xml',
             'index.html'
           ]
         }, {
